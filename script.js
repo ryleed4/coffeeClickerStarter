109f3536b7b187954550bcbe0a852ba6cad0cd7d
rylee <ryfrogquinn@gmail.com>
2023-03-01 13:30:44 -0600

    Completed coffee clicker assignment

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ const data = window.data;
 // Make your References to the two DOM nodes
 
 // Create a reference to the element who's ID is 'big_coffee and call it bigCoffee
-
+const bigCoffee = document.getElementById('big_coffee');
 // Create a reference to the element who's ID is 'producer_container' and call it producerContainer
-
+let producerContainer = document.getElementById('producer_container');
 /**************
  *   SLICE 1
  **************/
 
 function updateCoffeeView(coffeeQty) {
   // Create a reference to the element who's ID is 'coffee_counter'
+  let coffeeCounter = document.getElementById('coffee_counter');
+  //console.log(coffeeCounter)
   // Set the innerText of that element to be the coffeeQty passed into this function
+  coffeeCounter.innerText = coffeeQty;
 }
 
 function clickCoffee(data) {
   // Increment the data object's (passed into this function) coffee property by one
+  data.coffee += 1
   // call the updateCoffeeView function and pass it the newly updated data.coffee property
+  updateCoffeeView(data.coffee);
   // call the renderProducers function and pass it the data object
+  renderProducers(data);
 }
 
 /**************
@@ -28,14 +34,22 @@ function clickCoffee(data) {
 
 function unlockProducers(producers, coffeeCount) {
   // loop through the producers array passed into the function
+   for(let element of producers) {
   // for each producer, if the coffeeCount (passed in) is greater than or equal
   // to half the producer's price, reassign the producers.unlocked property to equal true
+    if(coffeeCount >= (element.price / 2)) {
+      element.unlocked = true;
+    }
+  }
 }
 
 function getUnlockedProducers(data) {
   // use the Array.prototype.filter() method
   // filter through the data.producers property, and return an array with only the producers whose
   // unlocked property is true
+  //console.log('The data is ', data)
+  const result = data.producers.filter(producer => producer.unlocked === true)
+  return result;
 }
 
 // You do not need to edit this function
@@ -76,10 +90,13 @@ function deleteAllChildNodes(parent) {
 
 function renderProducers(data) {
   // call the unlockProducers function and pass it data.producers and data.coffee
+  unlockProducers(data.producers, data.coffee);
 
   // make a reference to the DOM element whose ID is producer_container
+  let producerContainer = document.getElementById('producer_container')
 
   // call the deleteAllChildNodes function and pass it the above producerContainer element
+  deleteAllChildNodes(producerContainer);
 
   // you do not need to edit the following code, but for understanding, this gets the unlocked producers,
   // and for each producer makes a little html div with that producer's info
@@ -141,12 +158,15 @@ function buyButtonClick(event, data) {
     }
   }
 }
+ 
 
 function tick(data) {
   // increment the data object's (passed into this function)
   // coffee property by the data.totalCPS amount
+  data.coffee += data.totalCPS
 
   // call the updateCoffeeView function and pass it the data.coffee property
+  updateCoffeeView(data.coffee);
 
   // call the renderProducers function and pass it the newly updated data object
   renderProducers(data);
@@ -156,9 +176,12 @@ function tick(data) {
 
 // add a 'click' event listener to the bigCoffee element (that you referenced above)
 // the event listener should call the clickCoffee function, and pass in the global data object
+bigCoffee.addEventListener('click', () => clickCoffee(data));
 
 // add a 'click' event listener to the element (referenced at the top of the file)
 // the event listener should call the buyButtonClick function and pass it the event, and the global data object
-
+producerContainer.addEventListener('click', event => {
+  buyButtonClick(event, data);
+})
 // You do not need to edit this last line. This simple runs your tick function every 1000ms, or 1s
 setInterval(() => tick(data), 1000);
